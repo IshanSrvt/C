ea9334de7c537ba1d566523e567e0a0a25ae37a6
IshanSrvt <76547283+IshanSrvt@users.noreply.github.com>
2021-04-20 15:02:00 +0530

    Add files via upload

---
+++ Hello_World/Hello_World.cpp
@@ -0,0 +1,11 @@
+//Like & Subscribe to Mr.Codesr
+
+#include <stdio.h>
+
+int main(){
+    // printf() displays the string inside quotation
+    printf("Hello World!\n");
+    return 0;
+}
+
+// first build then run the code
